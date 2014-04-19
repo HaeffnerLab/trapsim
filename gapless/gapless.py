594a5888c802e45b3ff53247676e239c2ca0699c
Dylan J MacDonald Gorman <dylan@nfs.haeffnerlab.edu>
2014-04-19 13:20:49 -0700

    Numerical derivatives now with numdifftools throughout

--- gapless/gapless.py
+++ gapless/gapless.py
@@ -21,15 +21,32 @@ class Electrode():
 
         (self.x1, self.y1) = (xmin, ymin)
         (self.x2, self.y2) = (xmax, ymax)
-        self.d = d # step size for derivative
+        self.sub_electrodes = [] # list containing extra electrodes are connected to the current one
 
-    def solid_angle(self, xp, yp, zp):
+    def solid_angle(self, r):
         '''
         The solid angle for an arbitary rectangle oriented along the grid is calculated by
         Gotoh, et al, Nucl. Inst. Meth., 96, 3
+
+        The solid angle is calculated from the current electrode, plus any additional electrodes
+        that are electrically connected to the current electrode. This allows you to join electrodes
+        on the trap, or to make more complicated electrode geometries than just rectangles.
         '''
+
+        xp, yp, zp = r
         term = lambda x,y: np.arctan(((x - xp)*(y - yp))/(zp*np.sqrt( (x - xp)**2 + (y - yp)**2 + zp**2 )))
-        return abs(term(self.x2, self.y2) - term(self.x1, self.y2) - term(self.x2, self.y1) + term(self.x1, self.y1))
+        solid_angle = abs(term(self.x2, self.y2) - term(self.x1, self.y2) - term(self.x2, self.y1) + term(self.x1, self.y1))
+
+        for elec in self.sub_electrodes:
+            solid_angle += elec.solid_angle(r)
+
+    def extend(self, locations):
+        '''
+        Extend the current electrode by a set of rectangular regions
+        '''
+        for l in locations:
+            elec = Electrode(l)
+            self.sub_eub_electrodes.append(elec)
 
     def compute_voltage(self, r):
         '''
@@ -39,9 +56,7 @@ class Electrode():
         Also since the charge is e, the potential energy due to this potential is already in eV
         '''
 
-        [xp, yp, zp] = r
-
-        return (self.voltage/(2*np.pi))*self.solid_angle(xp, yp, zp)
+        return (self.voltage/(2*np.pi))*self.solid_angle(r)
 
     def compute_electric_field(self, r):
         '''
@@ -118,11 +133,10 @@ class World():
     '''
     Compute all electrodes
     '''
-    def __init__(self, d=10e-9):
+    def __init__(self):
         self.electrode_dict = {}
         self.rf_electrode_dict = {}
         self.dc_electrode_dict = {}
-        self.d = d # step-size for derivative
 
     def add_electrode(self, name, xr, yr, kind, voltage = None):
         '''
@@ -146,19 +160,19 @@ class World():
     def set_omega_rf(self, omega_rf):
         self.omega_rf = omega_rf
 
-    def compute_total_rf_voltage(self, xp, yp, zp):
+    def compute_total_rf_voltage(self, r):
         v = 0
         for e in self.rf_electrode_dict.keys():
-            v += self.compute_voltage(e, xp, yp, zp)
+            v += self.compute_voltage(e, r)
         return v
 
-    def compute_total_dc_voltage(self, xp, yp, zp):
+    def compute_total_dc_voltage(self, r):
         v = 0
         for e in self.dc_electrode_dict.keys():
             v += self.compute_voltage(e, xp, yp, zp)
         return v # the potential energy is automatically electron volts
 
-    def compute_rf_field(self, xp, yp, zp):
+    def compute_rf_field(self, r):
 
         '''
         Just add the electric field due to all the rf electrodes
@@ -167,39 +181,36 @@ class World():
         Ey = 0
         Ez = 0
         for name in self.rf_electrode_dict.keys():
-            [ex, ey, ez] = self.rf_electrode_dict[name].compute_electric_field(xp, yp, zp)
+            [ex, ey, ez] = self.rf_electrode_dict[name].compute_electric_field(r)
             Ex += ex
             Ey += ey
             Ez += ez
         return [Ex, Ey, Ez]
 
-    def compute_squared_field_amplitude(self, xp, yp, zp):
-        Ex, Ey, Ez = self.compute_rf_field(xp,yp,zp)
+    def compute_squared_field_amplitude(self, r):
+        Ex, Ey, Ez = self.compute_rf_field(r)
         
         return Ex**2 + Ey**2 + Ez**2 # has units of V^2/m^2
 
-    def compute_pseudopot(self, xp, yp, zp):
+    def compute_pseudopot(self, r):
         q = 1.60217657e-19 # electron charge
         m = 6.64215568e-26 # 40 amu in kg
         omega_rf = self.omega_rf
         joule_to_ev = 6.24150934e18 # conversion factor to take joules -> eV
-
-        E2 = self.compute_squared_field_amplitude(xp, yp, zp)
-
+        E2 = self.compute_squared_field_amplitude(r)
         return (q**2/(4*m*omega_rf**2))*E2*joule_to_ev # psuedopotential in eV
 
-    def compute_pseudopot_frequencies(self, xp, yp, zp):
+    def compute_pseudopot_frequencies(self, r):
         '''
         This is only valid if xp, yp, zp is the trapping position. Return frequency (i.e. 2*pi*omega)
         '''
-        d = self.d
-        U0 = self.compute_pseudopot(xp, yp, zp)
+        U0 = self.compute_pseudopot(r)
         ev_to_joule = 1.60217657e-19
         m = 6.64215568e-26 # 40 amu in kg
-        d2Udx2 = ev_to_joule*(self.compute_pseudopot(xp + d, yp, zp) - 2*U0 + self.compute_pseudopot(xp - d, yp, zp))/(d**2)
-        d2Udy2 = ev_to_joule*(self.compute_pseudopot(xp, yp + d, zp) - 2*U0 + self.compute_pseudopot(xp, yp -d, zp))/(d**2)
-        d2Udz2 = ev_to_joule*(self.compute_pseudopot(xp, yp, zp + d) - 2*U0 + self.compute_pseudopot(xp, yp, zp -d ))/(d**2)
-        
+        hessdiag = nd.Hessdiag( self.compute_pseudopot )(r)
+        d2Udx2 = ev_to_joule*hessdiag[0]
+        d2Udy2 = ev_to_joule*hessdiag[1]
+        d2Udy2 = ev_to_joule*hessdiag[2]
         '''
         Now d2Udx2 has units of J/m^2. Then w = sqrt(d2Udx2/(mass)) has units of angular frequency
         '''
@@ -209,17 +220,17 @@ class World():
         fz = np.sqrt(abs(d2Udz2)/m)/(2*np.pi)
         return [fx, fy, fz]
 
-    def compute_dc_potential_frequencies(self, xp, yp, zp):
+    def compute_dc_potential_frequencies(self, r):
         '''
         As always, this is valid only at the trapping position. Return frequency (not angular frequency)
         '''
         joule_to_ev = 6.24150934e18 # conversion factor to take joules -> eV
         m = 6.64215568e-26 # 40 amu in kg
-        U0 = self.compute_dc_voltage(xp, yp, zp)
-        d2Udx2 = ev_to_joule*(self.compute_dc_voltage(xp + d, yp, zp) - 2*U0 + self.compute_dc_voltage(xp - d, yp, zp))/(d**2)
-        d2Udy2 = ev_to_joule*(self.compute_dc_voltage(xp, yp + d, zp) - 2*U0 + self.compute_dc_voltage(xp, yp -d, zp))/(d**2)
-        d2Udz2 = ev_to_joule*(self.compute_dc_voltage(xp, yp, zp + d) - 2*U0 + self.compute_dc_voltage(xp, yp, zp -d ))/(d**2)
-
+        
+        hessdiag = nd.Hessdiag( self.compute_dc_voltage )(r)
+        d2Udx2 = ev_to_joule*hessdiag[0]
+        d2Udy2 = ev_to_joule*hessdiag[1]
+        d2Udz2 = ev_to_joule*hessdiag[2]
         fx = np.sqrt(abs(d2Udx2)/m)/(2*np.pi)
         fy = np.sqrt(abs(d2Udy2)/m)/(2*np.pi)
         fz = np.sqrt(abs(d2Udz2)/m)/(2*np.pi)
